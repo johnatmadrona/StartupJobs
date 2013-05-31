dd526a144726261ea0ffbc514583f063a6df7cd3
Hakon <mrhakon@gmail.com>
2013-05-31 00:49:34 -0700

    Add Impinj and AdReady

---
+++ StartupJobsParser/Scrapers/SjpAdReadyScraper.cs
@@ -0,0 +1,55 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace StartupJobsParser
+{
+    public class SjpAdReadyScraper : SjpScraper
+    {
+        private Uri _defaultUri = new Uri("https://www.smartrecruiters.com/AdReadyInc");
+        public override string CompanyName { get { return "AdReady"; } }
+        public override Uri DefaultUri
+        {
+            get { return _defaultUri; }
+        }
+
+        public SjpAdReadyScraper(string storageDirPath, ISjpIndex index)
+            : base(storageDirPath, index)
+        {
+        }
+
+        protected override IEnumerable<JobDescription> GetJds(Uri uri)
+        {
+            HtmlDocument doc = SjpUtils.GetHtmlDoc(uri);
+            foreach (HtmlNode jdInfoNode in doc.DocumentNode.SelectNodes("//table[@class='cs_table']/tr[@class='cs_container']"))
+            {
+                HtmlNode jdLink = jdInfoNode.SelectSingleNode("td[@class='cs_table']/a");
+                yield return GetAdReadyJd(new Uri(uri, jdLink.Attributes["href"].Value));
+            }
+        }
+
+        private JobDescription GetAdReadyJd(Uri uri)
+        {
+            HtmlNode jdNode = SjpUtils.GetHtmlDoc(uri).DocumentNode;
+
+            string title = jdNode.SelectSingleNode("//h1[@class='jobAdTitle']").InnerText;
+            string location = jdNode.SelectSingleNode("//span[@class='jobAdLocation']").InnerText;
+
+            HtmlNode descriptionNode = jdNode.SelectSingleNode("//div[@class='jobAdLeft']");
+            HtmlNode remove = descriptionNode.SelectSingleNode("input");
+            remove.ParentNode.RemoveChild(remove, false);
+            string description = descriptionNode.InnerHtml;
+
+            return new JobDescription()
+            {
+                SourceUri = uri.AbsoluteUri,
+                Company = CompanyName,
+                Title = WebUtility.HtmlDecode(title).Trim(),
+                Location = WebUtility.HtmlDecode(location).Trim(),
+                FullTextDescription = WebUtility.HtmlDecode(description).Trim(),
+                FullHtmlDescription = description
+            };
+        }
+    }
+}
---
+++ StartupJobsParser/Scrapers/SjpImpinjScraper.cs
@@ -0,0 +1,70 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace StartupJobsParser
+{
+    public class SjpImpinjScraper : SjpScraper
+    {
+        private Uri _defaultUri = new Uri("http://www.hirebridge.com/jobseeker2/Searchjobresults.asp?cid=6387");
+        public override string CompanyName { get { return "Impinj"; } }
+        public override Uri DefaultUri
+        {
+            get { return _defaultUri; }
+        }
+
+        public SjpImpinjScraper(string storageDirPath, ISjpIndex index)
+            : base(storageDirPath, index)
+        {
+        }
+
+        protected override IEnumerable<JobDescription> GetJds(Uri uri)
+        {
+            HtmlDocument doc = SjpUtils.GetHtmlDoc(uri);
+            foreach (HtmlNode jdLink in doc.DocumentNode.SelectNodes("//td[@class='SearchResultRow']/a"))
+            {
+                yield return GetImpinjJd(new Uri(uri, jdLink.Attributes["href"].Value));
+            }
+        }
+
+        private JobDescription GetImpinjJd(Uri uri)
+        {
+            HtmlNode jdNode = SjpUtils.GetHtmlDoc(uri).DocumentNode;
+
+            string title = jdNode.SelectSingleNode("html/body/table/tr/td[@class='InteriorHeader']").InnerText;
+            
+            string location = "Seattle, WA";
+            foreach (HtmlNode entry in jdNode.SelectNodes("//table[@class='InteriorTable']/tr/td"))
+            {
+                if (entry.InnerText.Trim().StartsWith("Location:", StringComparison.OrdinalIgnoreCase))
+                {
+                    HtmlNode next = entry.NextSibling;
+                    while (!next.OriginalName.Equals("td", StringComparison.OrdinalIgnoreCase))
+                    {
+                        next = next.NextSibling;
+                    }
+                    location = next.InnerText;
+                    break;
+                }
+            }
+
+            HtmlNode descriptionNode = jdNode.SelectSingleNode("//table[@class='InteriorPage']");
+            foreach (HtmlNode remove in descriptionNode.SelectNodes("//*[@class='button2']"))
+            {
+                remove.ParentNode.RemoveChild(remove, false);
+            }
+            string description = descriptionNode.InnerHtml;
+
+            return new JobDescription()
+            {
+                SourceUri = uri.AbsoluteUri,
+                Company = CompanyName,
+                Title = WebUtility.HtmlDecode(title).Trim(),
+                Location = WebUtility.HtmlDecode(location).Trim(),
+                FullTextDescription = WebUtility.HtmlDecode(description).Trim(),
+                FullHtmlDescription = description
+            };
+        }
+    }
+}
