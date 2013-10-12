dfb37df2ebe8a12d0b37b5d7b3a3c6c8fded6cc8
Hakon <mrhakon@gmail.com>
2013-10-11 23:11:41 -0700

    Fix 2nd Watch URL

--- StartupJobsParser/Scrapers/Sjp2ndWatchScraper.cs
+++ StartupJobsParser/Scrapers/Sjp2ndWatchScraper.cs
@@ -4,7 +4,7 @@ namespace StartupJobsParser
 {
     public class Sjp2ndWatchScraper : SjpResumatorScraperBase
     {
-        private Uri _publicUri = new Uri("http://jobs.rover.com/");
+        private Uri _publicUri = new Uri("http://2ndwatch.com/contact-us/careers/");
         private Uri _defaultUri = new Uri("http://app.theresumator.com/widgets/basic/create/2ndwatch");
 
         public override string CompanyName { get { return "2nd Watch"; } }
