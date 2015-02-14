ef87fddbc10aad2389801bc8b48df9dbb5eacc13
Hakon <mrhakon@gmail.com>
2015-02-13 16:37:00 -0800

    Add Opal scraper

--- StartupJobsParserConsoleApp/Program.cs
+++ StartupJobsParserConsoleApp/Program.cs
@@ -123,6 +123,7 @@ namespace StartupJobsParserConsoleApp
             scrapers.Add(new SjpMercentScraper(scraperParams));
             scrapers.Add(new SjpMozScraper(scraperParams));
             scrapers.Add(new SjpMixpoScraper(scraperParams));
+            scrapers.Add(new SjpOpalScraper(scraperParams));
             scrapers.Add(new SjpPeachScraper(scraperParams));
             scrapers.Add(new SjpPlacedScraper(scraperParams));
             scrapers.Add(new SjpWonderWorkshopScraper(scraperParams));
