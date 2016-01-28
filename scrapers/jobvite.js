36aeb8530f28d25f3f4d32c10b443188bdc0768a
Hakon <mrhakon@gmail.com>
2016-01-27 19:05:47 -0800

    Move common scraper functionality into a module

--- scrapers/jobvite.js
+++ scrapers/jobvite.js
@@ -2,12 +2,13 @@ var _q = require('q');
 var _request = require('request');
 var _cheerio = require('cheerio');
 var _node_url = require('url');
+var _util = require('./scraper_utils.js');
 
 function scrape(log, company, jvId) {
     var url = 'http://jobs.jobvite.com/careers/' + jvId + '/jobs?jvi=';
     var d = _q.defer();
 
-    log.info('Getting jd links from ' + url);
+    log.info({ company: company, jvId: jvId, url: url }, 'Getting jd links');
     _request(url, function(err, res, html) {
         if (err) {
             d.reject(err);
@@ -29,15 +30,8 @@ function scrape(log, company, jvId) {
     return d.promise;
 }
 
-function getHashableText(name) {
-    return name.replace(/[\s\.]+/g, '-').toLowerCase();
-}
-
-var _city_lookup = require('./city_lookup_map.json');
-var _state_lookup = require('./state_lookup_map.json');
-var _country_lookup = require('./country_lookup_map.json');
 function scrapeJobDescription(log, company, url) {
-    log.info('Getting jd from ' + url);
+    log.info({ company: company, url: url }, 'Getting jd');
 
     var d = _q.defer();
 
@@ -46,62 +40,20 @@ function scrapeJobDescription(log, company, url) {
             d.reject(err);
         } else {
             var $ = _cheerio.load(html);
+            var loc = _util.scrub_string($('.jv-job-detail-meta').contents()[2].data);
             var jd = {
                 url: url,
                 company: company,
-                title: scrubString($('.jv-header').text()),
-                location: {
-                    raw: scrubString($('.jv-job-detail-meta').contents()[2].data)
-                },
-                text: scrubString($('.jv-job-detail-description').text()),
+                title: _util.scrub_string($('.jv-header').text()),
+                location: _util.map_location(log, loc),
+                text: _util.scrub_string($('.jv-job-detail-description').text()),
                 html: $('.jv-job-detail-description').html()
             };
-
-            var loc_parts = jd.location.raw.split(',');
-            for (var i = 0; i < loc_parts.length; i++) {
-                var loc = getHashableText(loc_parts[i]);
-                var city, state;
-                if (_city_lookup[loc]) {
-                    city = _city_lookup[loc];
-                    state = _state_lookup[city.state];
-                    jd.location.city = city.canonicalName;
-                    jd.location.state = state.canonicalName;
-                    jd.location.country = _country_lookup[state.country].canonicalName;
-                    break;
-                } else if (_state_lookup[loc]) {
-                    state = _state_lookup[loc];
-                    jd.location.state = state.canonicalName;
-                    jd.location.country = _country_lookup[state.country].canonicalName;
-                    break;
-                } else if (_country_lookup[loc]) {
-                    jd.location.country = _country_lookup[loc].canonicalName;
-                    break;
-                }
-            }
-
-            // If we couldn't find the location, set the city to the unknown value
-            if (typeof(jd.location.country) === 'undefined') {
-                log.warn(
-                    { location: rawJob.Location, id: key, company: rawJob.Company, title: rawJob.Title },
-                    'Location not found in lookup maps'
-                );
-                jd.location.city = rawJob.Location;
-            }
-
             d.resolve(jd);
         }
     });
 
     return d.promise;
-}
-
-function scrubString(text) {
-    text = text.replace('\\\'', '\'');
-    text = text.replace('\\"', '"');
-    text = text.replace('\\n', ' ');
-    text = text.replace('\\r', ' ');
-    text = text.replace(/\s+/g, ' ');
-    return text.trim();
 }
 
 module.exports = {
---
+++ scrapers/scraper_utils.js
@@ -0,0 +1,57 @@
+var _city_lookup = require('./city_lookup_map.json');
+var _state_lookup = require('./state_lookup_map.json');
+var _country_lookup = require('./country_lookup_map.json');
+
+function get_hashable_text(name) {
+    return name.replace(/[\s\.]+/g, '-').toLowerCase();
+}
+
+function map_location(log, raw_location) {
+	var mapped = {
+		raw: raw_location
+	};
+
+	var loc_parts = raw_location.split(',');
+	for (var i = 0; i < loc_parts.length; i++) {
+	    var key = get_hashable_text(loc_parts[i]);
+	    var city, state;
+	    if (_city_lookup[key]) {
+	        city = _city_lookup[key];
+	        state = _state_lookup[city.state];
+	        mapped.city = city.canonicalName;
+	        mapped.state = state.canonicalName;
+	        mapped.country = _country_lookup[state.country].canonicalName;
+	        break;
+	    } else if (_state_lookup[key]) {
+	        state = _state_lookup[key];
+	        mapped.state = state.canonicalName;
+	        mapped.country = _country_lookup[state.country].canonicalName;
+	        break;
+	    } else if (_country_lookup[key]) {
+	        mapped.country = _country_lookup[key].canonicalName;
+	        break;
+	    }
+	}
+
+	// If we couldn't find the location, set the city to the unknown value
+	if (typeof(mapped.country) === 'undefined') {
+	    log.warn({ raw_location: raw_location }, 'Location not found in lookup maps');
+	    mapped.city = raw_location;
+	}
+
+	return mapped;
+}
+
+function scrub_string(text) {
+    var result = text.replace('\\\'', '\'');
+    result = result.replace('\\"', '"');
+    result = result.replace('\\n', ' ');
+    result = result.replace('\\r', ' ');
+    result = result.replace(/\s+/g, ' ');
+    return result.trim();
+}
+
+module.exports = {
+	map_location: map_location,
+	scrub_string: scrub_string
+};
