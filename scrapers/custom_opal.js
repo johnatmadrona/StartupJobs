16b26685f0c58eaaf933fab4a4f9e3aa212e28f1
Hakon <mrhakon@gmail.com>
2016-02-09 00:54:12 -0800

    Add Opal scraper

---
+++ scrapers/custom_opal.js
@@ -0,0 +1,59 @@
+var _q = require('q');
+var _request = require('request');
+var _cheerio = require('cheerio');
+var _node_url = require('url');
+var _util = require('./scraper_utils.js');
+
+function scrape(log, company, url) {
+    var d = _q.defer();
+
+    log.info({ company: company, url: url }, 'Getting jds');
+    _request(url, function(err, res, html) {
+        if (err) {
+            d.reject(err);
+        } else {
+            var $ = _cheerio.load(html);
+            var jds = [];
+            $('.career a').each(function() {
+                var title = _util.scrub_string($(this).find('h3').text());
+                var location = _util.scrub_string($(this).find('.location-name').text());
+                var jd_url = _node_url.resolve(url, $(this).attr('href'));
+	            jds.push(scrapeJobDescription(log, company, title, location, jd_url));
+            });
+            d.resolve(_q.all(jds));
+        }
+    });
+
+    return d.promise;
+}
+
+function scrapeJobDescription(log, company, title, location, url) {
+    var d = _q.defer();
+
+    log.info({ company: company, url: url }, 'Getting jd');
+    _request(url, function(err, res, html) {
+        if (err) {
+            d.reject(err);
+        } else {
+            var $ = _cheerio.load(html);
+            var description = _cheerio.load('<span></span>')('span')
+                .append($('.the-position'))
+                .append($('.the-requirements'));
+            var jd = {
+                url: url,
+                company: company,
+                title: title,
+                location: _util.map_location(log, location),
+                text: _util.scrub_string(description.text()),
+                html: description.html().trim()
+            };
+            d.resolve(jd);
+        }
+    });
+
+    return d.promise;
+}
+
+module.exports = {
+    scrape: scrape
+};
