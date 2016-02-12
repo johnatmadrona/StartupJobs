f95405b1c398be0ac6d8e6045e2ad016a3a02160
Hakon <mrhakon@gmail.com>
2016-02-11 21:35:26 -0800

    Update angellist and jobvite scraper to use util request function

--- scrapers/angellist.js
+++ scrapers/angellist.js
@@ -1,76 +1,47 @@
 var _q = require('q');
 var _node_url = require('url');
-var _request = require('request');
 var _cheerio = require('cheerio');
 var _util = require('./scraper_utils.js');
 
-var _al_headers = {
-    'Accept': 'text/html',
-    'Cache-Control': 'max-age=0',
-    'Host': 'angel.co',
-    'User-Agent': 'startup-jobs'
-};
-
 function scrape(log, company, angellist_id) {
-    var d = _q.defer();
+    var url = 'https://angel.co/' + angellist_id + '/jobs/';
+    log.info({ company: company, angellist_id: angellist_id, url: url }, 'Getting jd links');
+    return _util.request(log, url).then(function(html) {
+        var $ = _cheerio.load(html);
+        var listings = $('.listing');
+        var jds = [];
 
-    var options = {
-        url: 'https://angel.co/' + angellist_id + '/jobs/',
-        headers: _al_headers
-    };
+        log.info('Found ' + listings.length + ' listings');
+        listings.each(function() {
+            var titleNode = $(this).find('.title > a');
+            var locationNode = $(this).find('.job-data').eq(1);
+            jds.push(scrape_job_description(
+                log,
+                company,
+                _util.scrub_string(titleNode.text()),
+                _util.scrub_string(locationNode.text()),
+                _node_url.resolve(url, titleNode.attr('href'))
+            ));
+        });
 
-    log.info({ company: company, angellist_id: angellist_id, url: options.url }, 'Getting jd links');
-    _request.get(options, function(err, res, html) {
-        if (err) {
-            d.reject(err);
-        } else {
-            var $ = _cheerio.load(html);
-            var listings = $('.listing');
-            var jds = [];
-
-            log.info('Found ' + listings.length + ' listings');
-            listings.each(function() {
-                var titleNode = $(this).find('.title > a');
-                var locationNode = $(this).find('.job-data').eq(1);
-                jds.push(scrapeJobDescription(
-                    log,
-                    company,
-                    _util.scrub_string(titleNode.text()),
-                    _util.scrub_string(locationNode.text()),
-                    _node_url.resolve(options.url, titleNode.attr('href'))
-                ));
-            });
-
-            d.resolve(_q.all(jds));
-        }
+        return _q.all(jds);
     });
-
-    return d.promise;
 }
 
-function scrapeJobDescription(log, company, title, location, url) {
-    var d = _q.defer();
-
+function scrape_job_description(log, company, title, location, url) {
     log.info({ company: company, title: title, location: location, url: url }, 'Getting jd');
-    _request({ url: url, headers: _al_headers }, function(err, res, html) {
-        if (err) {
-            d.reject(err);
-        } else {
-            var $ = _cheerio.load(html);
-            var jd = _util.create_jd(
-                log,
-                url,
-                company,
-                title,
-                location,
-                _util.scrub_string($('.product-info').text()),
-                $('.product-info').html().trim()
-            );
-            d.resolve(jd);
-        }
+    return _util.request(log, url).then(function(html) {
+        var $ = _cheerio.load(html);
+        return _util.create_jd(
+            log,
+            url,
+            company,
+            title,
+            location,
+            _util.scrub_string($('.product-info').text()),
+            $('.product-info').html().trim()
+        );
     });
-
-    return d.promise;
 }
 
 module.exports = {
--- scrapers/jobvite.js
+++ scrapers/jobvite.js
@@ -1,51 +1,26 @@
 var _q = require('q');
-var _request = require('request');
 var _cheerio = require('cheerio');
 var _node_url = require('url');
 var _util = require('./scraper_utils.js');
 
 function scrape(log, company, url) {
-    var d = _q.defer();
-
     var parsed_url = _node_url.parse(url);
     if (parsed_url.hostname !== 'jobs.jobvite.com' &&
         parsed_url.hostname !== 'hire.jobvite.com') {
-        d.reject(new Error('Invalid domain for jobvite scraping: ' + parsed_url.hostname));
-        return;
+        return _q.reject(new Error('Invalid domain for jobvite scraping: ' + parsed_url.hostname));
     }
 
     log.info({ company: company, url: url }, 'Getting jd links');
-    _request({ url: url, followRedirect: true }, function(err, res, html) {
-        if (err) {
-            d.reject(err);
-        } else if (!are_urls_equal(url, res.request.uri.href, true)) {
-            log.error({ original_url: url, redirected_url: res.request.uri.href }, 'URL redirected');
-            d.reject(new Error('Request redirected from ' + url + ' to ' + res.request.uri.href));
-        } else {
-            d.resolve(parse_listing_and_scrape_jds(log, company, parsed_url, html));
-        }
+    return _util.request(log, url).then(function(html) {
+        return parse_listing_and_scrape_jds(log, company, parsed_url, html);
     });
-
-    return d.promise;
-}
-
-function are_urls_equal(first, second, ignore_protocol) {
-    var f = first;
-    var s = second;
-    if (typeof(ignore_protocol) !== 'undefined' && ignore_protocol) {
-        f_start = first.indexOf('//');
-        f = f.substring(f_start);
-        s_start = second.indexOf('//');
-        s = s.substring(s_start);
-    }
-    return f === s;
 }
 
 function parse_listing_and_scrape_jds(log, company, parsed_url, html) {
     var jds = [];
 
     if (parsed_url.hostname === 'jobs.jobvite.com') {
-        return get_jobs_style_links(parsed_url, html).then(function(urls) {
+        return get_jobs_style_links(log, parsed_url, html).then(function(urls) {
             for (var i = 0; i < urls.length; i++) {
                 jds.push(scrape_job_description(log, company, urls[i]));
             }
@@ -79,11 +54,11 @@ function parse_listing_and_scrape_jds(log, company, parsed_url, html) {
         }
         return _q.all(jds); 
     } else {
-        throw new Error('Invalid domain for jobvite scraping: ' + parsed_url.hostname);
+        return _q.reject(new Error('Invalid domain for jobvite scraping: ' + parsed_url.hostname));
     }
 }
 
-function get_jobs_style_links(parsed_url, base_html) {
+function get_jobs_style_links(log, parsed_url, base_html) {
     var promises = [];
     var links = [];
 
@@ -94,24 +69,17 @@ function get_jobs_style_links(parsed_url, base_html) {
     });
 
     $base('a[href*="/search?"]').each(function() {
-        var d = _q.defer();
         var url = _node_url.resolve(parsed_url.href, $base(this).attr('href'));
-        _request(url, function(err, res, html) {
-            if (err) {
-                d.reject(err);
-            } else {
-                var $ = _cheerio.load(html);
-                var match = /(\d+)-(\d+) of (\d+)/.exec($('.jv-pagination-text').text().trim());
-                if (match[2] !== match[3]) {
-                    throw new Error('Pagination not yet implemented');
-                }
-                $('a[href*="/job/"]').each(function() {
-                    links.push(_node_url.resolve(parsed_url.href, $(this).attr('href')));
-                });
-                d.resolve();
+        promises.push(_util.request(log, url).then(function(html) {
+            var $ = _cheerio.load(html);
+            var match = /(\d+)-(\d+) of (\d+)/.exec($('.jv-pagination-text').text().trim());
+            if (match[2] !== match[3]) {
+                return _q.reject(new Error('Pagination not yet implemented'));
             }
-        });
-        promises.push(d.promise);
+            $('a[href*="/job/"]').each(function() {
+                links.push(_node_url.resolve(parsed_url.href, $(this).attr('href')));
+            });
+        }));
     });
 
     return _q.all(promises).then(function() {
@@ -155,67 +123,56 @@ function scrape_job_description(log, company, url) {
     var parsed_url = _node_url.parse(url);
     if (parsed_url.hostname !== 'jobs.jobvite.com' &&
         parsed_url.hostname !== 'hire.jobvite.com') {
-        throw new Error('Invalid domain for jobvite scraping: ' + parsed_url.hostname);
+        return _q.reject(new Error('Invalid domain for jobvite scraping: ' + parsed_url.hostname));
     }
 
-    var d = _q.defer();
-    _request(url, function(err, res, html) {
-        if (err) {
-            d.reject(err);
-        } else if (!are_urls_equal(url, res.request.uri.href, true)) {
-            log.error({ original_url: url, redirected_url: res.request.uri.href }, 'URL redirected');
-            d.reject(new Error('Request redirected from ' + url + ' to ' + res.request.uri.href));
-        } else {
-            var $ = _cheerio.load(html);
-            var title, location, content_node;
-            if (res.request.uri.host === 'jobs.jobvite.com') {
-                title = _util.scrub_string($('.jv-header').text());
-                location = _util.scrub_string($('.jv-job-detail-meta').contents()[2].data);
-                content_node = $('.jv-job-detail-description');
-            } else if (res.request.uri.host === 'hire.jobvite.com') {
-                var location_node;
-                if ($('.title_jobdesc').length > 0) {
-                    title = _util.scrub_string($('.title_jobdesc > h2').text());
-                    location_node = $('.title_jobdesc > h3');
-                    content_node = $('.jobDesc');
-                } else if ($('.jvjobheader').length > 0) {
-                    title = _util.scrub_string($('.jvjobheader > h2').text());
-                    location_node = $('.jvjobheader > h3');
-                    content_node = $('.jvdescriptionbody');
-                } else if ($('.jvheader').length > 0) {
-                    // This may be custom code for Indochino
-                    title = _util.scrub_string($('.jvheader').text());
-                    location_node = $('.jvheader').next();
-                    content_node = _cheerio.load('<span></span>')('span');
-                    $('.jvheader').parent().children('p, ul, h2').each(function() {
-                        content_node.append($(this).clone());
-                    });
-                } else if ($('.jvcontent').length > 0) {
-                    // This may be custom code for Redfin
-                    var title_node = $('.jvcontent h1');
-                    title = _util.scrub_string(title_node.text());
-                    location_node = title_node.parent().children('h2');
-                    content_node = $('.jobDesc');
-                } else {
-                    throw new Error('Unexpected format');
-                }
-                location = _util.scrub_string(location_node.text().split('|')[1]);
+    return _util.request(log, url).then(function(html) {
+        var $ = _cheerio.load(html);
+        var title, location, content_node;
+        if (parsed_url.hostname === 'jobs.jobvite.com') {
+            title = _util.scrub_string($('.jv-header').text());
+            location = _util.scrub_string($('.jv-job-detail-meta').contents()[2].data);
+            content_node = $('.jv-job-detail-description');
+        } else if (parsed_url.hostname === 'hire.jobvite.com') {
+            var location_node;
+            if ($('.title_jobdesc').length > 0) {
+                title = _util.scrub_string($('.title_jobdesc > h2').text());
+                location_node = $('.title_jobdesc > h3');
+                content_node = $('.jobDesc');
+            } else if ($('.jvjobheader').length > 0) {
+                title = _util.scrub_string($('.jvjobheader > h2').text());
+                location_node = $('.jvjobheader > h3');
+                content_node = $('.jvdescriptionbody');
+            } else if ($('.jvheader').length > 0) {
+                // This may be custom code for Indochino
+                title = _util.scrub_string($('.jvheader').text());
+                location_node = $('.jvheader').next();
+                content_node = _cheerio.load('<span></span>')('span');
+                $('.jvheader').parent().children('p, ul, h2').each(function() {
+                    content_node.append($(this).clone());
+                });
+            } else if ($('.jvcontent').length > 0) {
+                // This may be custom code for Redfin
+                var title_node = $('.jvcontent h1');
+                title = _util.scrub_string(title_node.text());
+                location_node = title_node.parent().children('h2');
+                content_node = $('.jobDesc');
+            } else {
+                throw new Error('Unexpected format');
             }
-
-            var jd = _util.create_jd(
-                log,
-                url,
-                company,
-                title,
-                location,
-                _util.scrub_string(content_node.text()),
-                content_node.html().trim()
-            );
-            d.resolve(jd);
+            location = _util.scrub_string(location_node.text().split('|')[1]);
         }
-    });
 
-    return d.promise;
+        return _util.create_jd(
+            log,
+            url,
+            company,
+            title,
+            location,
+            _util.scrub_string(content_node.text()),
+            content_node.html().trim()
+        );
+    });
 }
 
 module.exports = {
